02aabf50fe6ec151b4bd866e0e0427c6fe1cbdd7
artur-kompala <artur.kompala@gmail.com>
2023-11-14 01:55:42 +0100

    rating

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 import NavBar from "./NavBar";
 import Main from "./Main";
 
+
 const tempMovieData = [
   {
     imdbID: "tt1375666",
--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+//import './index.css';
+//import App from './App';
+import StarRating from './StarRating';
+
+function Test(){
+  const [movieRating,setMovieRating] = useState(0); 
+  return(
+    <>
+  <StarRating maxRating={5} className='test' messages={["Terrible","Bad","Okay","Good","Amazing"] } defaultRating={3} onSetRating={setMovieRating}></StarRating>
+  <h1>DUPA {movieRating}</h1>
+  </>
+  )
+
+  
+  
+  
+}
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   {/*<App />*/} 
+   <Test></Test>
   </React.StrictMode>
 );
 
